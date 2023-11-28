f45539a69e5327d3b574bc43a3e606f33f5aed45
Neil Supillo <neilsupillo@gmail.com>
2023-11-28 17:23:03 +0800

    sad

--- app.js
+++ app.js
@@ -180,7 +180,7 @@ app.get("/forget", function (req, res) {
 app.get("/secrets", async function (req, res) {
   console.log("secrets user " + req.user);
 
-  if (auth) {
+  if (!req.isAuthenticated()) {
     const foundUsers = await User.find({
       secrets: { $exists: true, $not: { $size: 0 } },
     });
@@ -209,7 +209,7 @@ app.get("/secrets", async function (req, res) {
 app.get("/submit", async function (req, res) {
   console.log("submit user " + req.user);
   //console.log(req);
-  if (auth) {
+  if (!req.isAuthenticated()) {
     const userId = await User.findById(req.user.id);
     // console.log("/submit " + userId);
     res.render("submit", { user: userId, wrong: "" });
